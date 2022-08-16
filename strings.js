f8cea9c37e07d3b11f789e3f3709b48c69212f54
Mahesh <itstribe.mahi@gmail.com>
2022-08-16 16:51:31 +0530

    string methods

---
+++ strings.js
@@ -0,0 +1,26 @@
+// // string to uppercase
+// var x="mahesh";
+
+// console.log(x.toUpperCase());
+
+// //string to lowercase
+// var x="maheSHB";
+
+// console.log(x.toLowerCase());
+
+//lower and upper
+// let x='here we are learning javascript';
+// let y= x.toUpperCase();
+// console.log(x);
+
+
+// replacing string with another
+var a='here are strugling';
+var b=a.replace("strugling","learning");
+console.log(b);
+
+//    dtring trim method/ only for front and back unwanted spaces
+var x='      my name Mahesh    Badanakurthi          ';
+var y=x.trim('Badanakurthi');
+console.log(y);
+
