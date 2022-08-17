b9cbb4ffed540cf578d5b2dcb98544a3ece0fee2
Mahesh <itstribe.mahi@gmail.com>
2022-08-17 16:30:11 +0530

    string methods

--- strings.js
+++ strings.js
@@ -15,12 +15,43 @@
 
 
 // replacing string with another
-var a='here are strugling';
-var b=a.replace("strugling","learning");
-console.log(b);
+// var a='here are strugling';
+// var b=a.replace("strugling","learning");
+// console.log(b);
 
 //    dtring trim method/ only for front and back unwanted spaces
-var x='      my name Mahesh    Badanakurthi          ';
-var y=x.trim('Badanakurthi');
-console.log(y);
+// var x='      my name Mahesh    Badanakurthi          ';
+// var y=x.trim('Badanakurthi');
+// console.log(y);
+
+// let x=1234;
+// let y=2345;
+// console.log(x++, x," ",++y);// 
+// let z=x.toString(); //i intentionally changing num to string
+// console.log(x+y);
+// console.log(x+z);
+
+
+
+let n='Here we are learning JavaScript';
+// console.log(n.slice(5,15));
+// console.log(n.substring(5,15)); //gives range between 5 to 15
+
+// console.log(n.split("",5)); // It splits the every element by single quotes
+// console.log(n.charAt(5));// to know the 5th place element
+
+
+let x = "some string";
+
+console.log(x.charAt(true)); // terue converted to 1
+console.log(x.charAt(NaN)); // converted to 0
+console.log(x.charAt(undefined));// bellow are converted to 0
+console.log(x.charAt([]))
+console.log(x.charAt(""))
+
+
+
+
+
+
 
